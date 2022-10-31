2f84d3d3d941281b8e62677dea7237fad3496c80
Akshat Singh <HackerDaGreat57@gmail.com>
2022-10-30 20:19:06 -0700

    Whoops. Add GNU-EFI files.

---
+++ bin/gnu-efi-3.0.6/lib/x86_64/efi_stub.d
@@ -0,0 +1,2 @@
+D:\3dos\bin\gnu-efi-3.0.6\lib\x86_64\efi_stub.o: \
+ D:\3dos\bin\gnu-efi-3.0.6\lib\x86_64\efi_stub.S
---
+++ bin/gnu-efi-3.0.6/lib/x86_64/setjmp.d
@@ -0,0 +1,2 @@
+D:\3dos\bin\gnu-efi-3.0.6\lib\x86_64\setjmp.o: \
+ D:\3dos\bin\gnu-efi-3.0.6\lib\x86_64\setjmp.S
